7f924f32f806449f265a8b8ac7bf28cf4f6b2760
MusaAlFarid <musaalfarid@gmail.com>
2021-10-26 08:57:43 +0700

    WIP-create kendaraan controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\User\UserMessageController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,8 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/test', [App\Http\Controllers\TestController::class, 'test']);
 
 Route::group(['prefix' => 'message'], function () {
-    Route::get('/all',[App\Http\Controllers\User\UserMessageController::class, 'get_chat_room'])->name('message.getall');
-    Route::get('/room/{id}',[App\Http\Controllers\User\UserMessageController::class, 'get_room_by_id'])->name('message.byid');
-    Route::get('/room/message/{chat_room_id}',[App\Http\Controllers\User\UserMessageController::class, 'get_message_by_room'])->name('message.byroom');
-    Route::post('/send',[App\Http\Controllers\User\UserMessageController::class, 'send_message'])->name('message.send');
+    Route::get('/all',[UserMessageController::class, 'get_chat_room'])->name('message.getall');
+    Route::get('/room/{id}',[UserMessageController::class, 'get_room_by_id'])->name('message.byid');
+    Route::get('/room/message/{chat_room_id}',[UserMessageController::class, 'get_message_by_room'])->name('message.byroom');
+    Route::post('/send',[UserMessageController::class, 'send_message'])->name('message.send');
 });
